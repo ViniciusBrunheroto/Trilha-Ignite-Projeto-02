3ccd2a1ef56a85835d781443f20ca7c611d92a37
Vinícius Brunheroto <vinibrunheroto12@gmail.com>
2024-04-14 20:33:26 -0300

    Update: App

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/global.ts'
 import { Router } from './Router.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CyclesContextProvider } from './contexts/CyclesContext.tsx'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter basename={'/Trilha-Ignite-Projeto-02/'}>
+      <HashRouter basename={'/Trilha-Ignite-Projeto-02/'}>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
-      </BrowserRouter>
+      </HashRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
